23bea168b72dd577a58d447e321df69ac274450a
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-12-10 16:57:57 -0300

    Added database helper and configs

--- config.php
+++ config.php
@@ -3,4 +3,10 @@
 define('BASE_PATH', '/social_music/backend/');
 define('CONTROLLERS_FOLDER', 'controllers/');
 define('HELPERS_FOLDER', 'helpers/');
+
+// DATABASE
+define('DB_NAME', 'social_music');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 ?>
---
+++ helpers/Database.php
@@ -0,0 +1,21 @@
+<?php
+class Database {
+  static function connect() {
+    $dsn = 'mysql:dbname=' . DB_NAME . ';host=' . DB_HOST;
+
+    try {
+      $conn = new PDO($dsn, DB_USER, DB_PASS);
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $conn;
+    } catch(PDOException $e) {
+      self::db_error($e);
+    }
+  }
+
+  static function db_error($error) {
+    $result['error']['message'] = 'Server error, please try again!';
+    $result['error']['database'] = $error;
+    Output::response($result, 500);
+  }
+}
+?>
