593051588698e56e40cc0d8ecaf4f7c53e593dde
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-12-11 18:46:31 -0300

    Table users structure changes

--- database/social_music.sql
+++ database/social_music.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2022 at 09:42 PM
+-- Generation Time: Dec 11, 2022 at 10:46 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -42,9 +42,17 @@ CREATE TABLE `sessions` (
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
   `email` varchar(150) NOT NULL,
   `pass` varchar(60) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `pass`) VALUES
+(16, 'Vítor Ambrizzi', 'vitorambrizzi@gmail.com', '$2y$12$rhAQh8aubsZIneg7TsCVyO0EMZQionQyJ8ZHs3d7teTMBibmHMwIu');
 
 --
 -- Indexes for dumped tables
@@ -72,13 +80,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `sessions`
 --
 ALTER TABLE `sessions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
