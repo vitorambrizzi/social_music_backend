e9b40c594f634da039e9f65ebd04fd038ad3ae64
Vítor Ambrizzi <ambrizzivitor@gmail.com>
2022-12-12 10:23:56 -0300

    Included CORS handling

--- config.php
+++ config.php
@@ -1,13 +1,18 @@
 <?php
-// PATHS AND FOLDERS
-define('BASE_PATH', '/social_music/backend/');
-define('CONTROLLERS_FOLDER', 'controllers/');
-define('HELPERS_FOLDER', 'helpers/');
-define('MODELS_FOLDER', 'models/');
+// CORS
+define('ALLOWED_HOSTS', '*');
+define('ALLOWED_METHODS', 'GET, POST, OPTIONS, DELETE, PUT');
+define('ALLOWED_HEADERS', 'Accept, Content-Type');
 
 // DATABASE
 define('DB_NAME', 'social_music');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
+
+// PATHS AND FOLDERS
+define('BASE_PATH', '/social_music/backend/');
+define('CONTROLLERS_FOLDER', 'controllers/');
+define('HELPERS_FOLDER', 'helpers/');
+define('MODELS_FOLDER', 'models/');
 ?>
--- helpers/Router.php
+++ helpers/Router.php
@@ -1,6 +1,8 @@
 <?php
 class Router {
   static function gate_keeper() {
+    self::handle_CORS();
+
     $uri = $_SERVER['REQUEST_URI'];
     $uri_clean = str_replace(BASE_PATH, '', $uri);
     $uri_segments = explode('/', $uri_clean);
@@ -25,6 +27,15 @@ class Router {
     if ($_SERVER['REQUEST_METHOD'] !== $request_method) {
       $result['error']['message'] = 'Method ' . $_SERVER['REQUEST_METHOD'] . ' not allowed for this route!';
       Output::response($result, 405);
+    }
+  }
+
+  static function handle_CORS() {
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+      header('Access-Control-Allow-Origin: ' . ALLOWED_HOSTS);
+      header('Access-Control-Allow-Methods: ' . ALLOWED_METHODS);
+      header('Access-Control-Allow-Headers: ' . ALLOWED_HEADERS);
+      die;
     }
   }
 }
